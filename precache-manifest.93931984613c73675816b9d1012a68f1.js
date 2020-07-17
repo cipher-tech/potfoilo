34b84c833bd7e16051fa00aa3b6dda2a9f7360a8
cipher-tech <nickchibuikem@gmail.com>
2020-07-17 02:00:55 +0100

    Updates

---
+++ precache-manifest.93931984613c73675816b9d1012a68f1.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff68ad11ff15fb5083bc064ead34c84",
+    "url": "/potfoilo/index.html"
+  },
+  {
+    "revision": "521b9877f1c437eb92de",
+    "url": "/potfoilo/static/css/2.605bd980.chunk.css"
+  },
+  {
+    "revision": "722df993ad640f370514",
+    "url": "/potfoilo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "521b9877f1c437eb92de",
+    "url": "/potfoilo/static/js/2.662ea582.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/potfoilo/static/js/2.662ea582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722df993ad640f370514",
+    "url": "/potfoilo/static/js/main.bc137d67.chunk.js"
+  },
+  {
+    "revision": "be386dae6c0b7dade5b7",
+    "url": "/potfoilo/static/js/runtime-main.cbf78d92.js"
+  },
+  {
+    "revision": "0bbd801f028993242ead7984535d407f",
+    "url": "/potfoilo/static/media/XIcon.0bbd801f.svg"
+  },
+  {
+    "revision": "71ad6429fd273363603ac9e4fd924949",
+    "url": "/potfoilo/static/media/aboutMeCurve.71ad6429.svg"
+  },
+  {
+    "revision": "f8720d192e614e87a7049659f2180188",
+    "url": "/potfoilo/static/media/aboutPic.f8720d19.JPG"
+  },
+  {
+    "revision": "10baff4c898ad2ba358e36ef829d1c60",
+    "url": "/potfoilo/static/media/adobe-xd.10baff4c.svg"
+  },
+  {
+    "revision": "9086960b926427485f6647fa421c677d",
+    "url": "/potfoilo/static/media/apollo-graphql-1.9086960b.svg"
+  },
+  {
+    "revision": "9acd921d83e16158b160e5e7405c73b5",
+    "url": "/potfoilo/static/media/avatar1.9acd921d.jpg"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/potfoilo/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "cee9a1558dd6b17d94f3125cb90050a3",
+    "url": "/potfoilo/static/media/circleSvg.cee9a155.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/potfoilo/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "229d89e9e14d02e872cd9979f7a53a29",
+    "url": "/potfoilo/static/media/devStack.229d89e9.svg"
+  },
+  {
+    "revision": "a72029118580a8768bbc08ff791db112",
+    "url": "/potfoilo/static/media/diamondSvg.a7202911.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/potfoilo/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "05c774d050318c2c977bf6e1582b5f8f",
+    "url": "/potfoilo/static/media/facebook.05c774d0.svg"
+  },
+  {
+    "revision": "148e428b37349868a18b9476a384c806",
+    "url": "/potfoilo/static/media/github.148e428b.svg"
+  },
+  {
+    "revision": "6270a2d4757a839d4653c02f3db32e2f",
+    "url": "/potfoilo/static/media/graphql.6270a2d4.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/potfoilo/static/media/html.676e8188.svg"
+  },
+  {
+    "revision": "6dce434ebf136aa5a236c9d2d7fc91e4",
+    "url": "/potfoilo/static/media/instagram.6dce434e.svg"
+  },
+  {
+    "revision": "a16e843934fd814b00b046af24e09741",
+    "url": "/potfoilo/static/media/invision-studio.a16e8439.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/potfoilo/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "499148cba7dc771d8f788722df48c4a6",
+    "url": "/potfoilo/static/media/jquery.499148cb.svg"
+  },
+  {
+    "revision": "2fa5a6b6d7ce810d2b67ebf6df87fcc8",
+    "url": "/potfoilo/static/media/laravel-2.2fa5a6b6.svg"
+  },
+  {
+    "revision": "11c5965059f35eaf307c4916570856ea",
+    "url": "/potfoilo/static/media/linkedin.11c59650.svg"
+  },
+  {
+    "revision": "60b16a4249ca1dca7367e40b13f3748c",
+    "url": "/potfoilo/static/media/logo.60b16a42.jpg"
+  },
+  {
+    "revision": "fe397f2a174dad6c5f2c36cec0c1ae5e",
+    "url": "/potfoilo/static/media/materialize.fe397f2a.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/potfoilo/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a02446035f7a7561254bb801800d1fa0",
+    "url": "/potfoilo/static/media/mongoose.a0244603.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/potfoilo/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "b5d25ca3c0decaea251b75c69a934e8b",
+    "url": "/potfoilo/static/media/next-js.b5d25ca3.svg"
+  },
+  {
+    "revision": "14051f0df35967767ea673812be60beb",
+    "url": "/potfoilo/static/media/node-js-logo.14051f0d.svg"
+  },
+  {
+    "revision": "c6c7d176373dfef933e91a7bead4420a",
+    "url": "/potfoilo/static/media/p1-1.c6c7d176.jpg"
+  },
+  {
+    "revision": "c35b9ae600b4872d032a2817f6d0877b",
+    "url": "/potfoilo/static/media/p2-2.c35b9ae6.jpg"
+  },
+  {
+    "revision": "7b629fba90d14b787f21c4386af4d34d",
+    "url": "/potfoilo/static/media/p3-3.7b629fba.jpg"
+  },
+  {
+    "revision": "8ba3896894be3e8da7005a93a12e827d",
+    "url": "/potfoilo/static/media/p5-5.8ba38968.jpg"
+  },
+  {
+    "revision": "d770e55577a7cd697aa4b7e614ed0c38",
+    "url": "/potfoilo/static/media/personSvg.d770e555.svg"
+  },
+  {
+    "revision": "7634d1a3c06643e9394adc4ba35d7d2e",
+    "url": "/potfoilo/static/media/php.7634d1a3.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/potfoilo/static/media/python-5.146716bd.svg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/potfoilo/static/media/react.c3fbd71b.svg"
+  },
+  {
+    "revision": "4aaff929af8a78f0a5e3c1b27a0b6725",
+    "url": "/potfoilo/static/media/redis.4aaff929.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/potfoilo/static/media/sass.b446ca5a.svg"
+  },
+  {
+    "revision": "43c9d125de48e59cb29a88dd1b20bc46",
+    "url": "/potfoilo/static/media/sequelize.43c9d125.svg"
+  },
+  {
+    "revision": "3748e14459da60cd4f4224e767dcbc98",
+    "url": "/potfoilo/static/media/servicesSvg.3748e144.svg"
+  },
+  {
+    "revision": "6a3a18db97a85590ce59580854e5d172",
+    "url": "/potfoilo/static/media/triangleSvg.6a3a18db.svg"
+  },
+  {
+    "revision": "c45f8f67572d820dd6925fae2c39e359",
+    "url": "/potfoilo/static/media/twitter.c45f8f67.svg"
+  },
+  {
+    "revision": "837b970d5c77507b2ea4bb0ec18059a0",
+    "url": "/potfoilo/static/media/webpack.837b970d.svg"
+  }
+]);
